103b21ee6ff171241682349ac9521ba073166f68
Ben Schmaltz <benschmaltz@gmail.com>
2015-02-04 21:09:00 -0500

    h2

--- web/routes/users.js
+++ web/routes/users.js
@@ -4,6 +4,7 @@ var express = require('express');
 var router = express.Router();
 var userHelper = require('./helpers/user-helper');
 
+
 //login
 router.post('/login', function(req, res) {
   var token = req.body.user.token
