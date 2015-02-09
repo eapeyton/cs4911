e5f2aaf5e52c6c46a55938d0d9e302f1916c7ddf
Rikin Marfatia <rikinm10@gmail.com>
2015-02-09 16:42:35 -0500

    initial commit

---
+++ mobile/Cards/app/src/main/java/cards/seniordesign/com/cards/MainActivity.java
@@ -0,0 +1,18 @@
+package cards.seniordesign.com.cards;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    
+
+}
