13ae710c620d09d7fde1758130ef618bf5a40634
Hunter Clarke <hunter@hunterc.com>
2015-02-09 15:44:56 -0500

    updates the socket code with dummy states

--- web/websocket.js
+++ web/websocket.js
@@ -2,8 +2,20 @@ var WebSocketHandler = function(io) {
   io.on('connection', function(socket) {
     socket.emit('message', 'hello world');
 
-    socket.on('ping', function() {
-      socket.emit('pong');
+    socket.on('start game', function(msg) {
+      // deal hands
+      // draw blank
+    });
+    socket.on('play',function(msg) {
+      // update state to judgement if applicable
+    });
+    socket.on('timeout', function(msg) {
+      // update state to judgement if applicable
+    });
+    socket.on('judgement', function(msg) {
+      // move to end game if applicable
+      // else update the hand count and draw new cards
+      // draw blank
     });
   });
 }
