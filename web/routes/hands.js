025437afff617646ead3492a91b5011df94443b0
Ben Schmaltz <benschmaltz@gmail.com>
2015-03-05 21:45:36 -0500

    tested play card2

---
+++ web/routes/hands.js
@@ -0,0 +1,44 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+var authorize = require('../lib/authorization-middleware')().authorize;
+
+
+//get cards in users hand
+router.get('/', authorize, function(req, res) {
+  models.Game.find({where:
+    {
+      roomId: req.authorizedUser.roomId,
+      finishTime: null
+    }
+  })
+  .then(function(game){
+    if(game === null){
+      throw "user not in game"
+    }
+    models.Hand.findAll({
+      where: {
+        userId: req.authorizedUser.id,
+        gameId: game.id
+      },
+      include:[{
+        model: models.Card
+      }]
+    })
+    .then(function(hand){
+      var cards = [];
+      for(var i=0; i<hand.length; i++){
+        cards.push(hand[i].Card);
+      }
+      res.json({
+        success: true,
+        cards: cards
+      });
+    });
+  })
+  .catch(function(errors){
+    res.json({success: false, errors: errors});
+  });
+});
+
+module.exports = router;
