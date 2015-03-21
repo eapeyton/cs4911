3530e0cfb2e39e9a9f46e31f3b89146be6ab61b2
Eric Peyton <ea.peyton@gmail.com>
2015-03-21 15:09:22 -0400

    Finish adding Facebook login button. Works now but not doing anything with result

--- mobile/Cards/app/src/main/java/cards/seniordesign/com/cards/MainActivity.java
+++ mobile/Cards/app/src/main/java/cards/seniordesign/com/cards/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
@@ -15,13 +16,29 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+    private MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
+
         Typeface blendaFont = Typeface.createFromAsset(getAssets(), "Blenda.otf");
         TextView splashText = (TextView) findViewById(R.id.splash_text);
         splashText.setTypeface(blendaFont);
