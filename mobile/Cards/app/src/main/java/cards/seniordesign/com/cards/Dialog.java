202e6fee577b18343521aa86ee293b9eb28a1e56
Eric Peyton <ea.peyton@gmail.com>
2015-04-11 02:15:36 -0400

    Add Dialog class for displaying error dialogs statically

---
+++ mobile/Cards/app/src/main/java/cards/seniordesign/com/cards/Dialog.java
@@ -0,0 +1,23 @@
+package cards.seniordesign.com.cards;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+/**
+ * Created by Eric on 4/10/15.
+ */
+public class Dialog {
+
+    public static void showError(Activity activity, String error) {
+        AlertDialog dialog =  new AlertDialog.Builder(activity)
+                .setMessage(error)
+                .setNeutralButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                            }
+                        })
+                .create();
+        dialog.show();
+    }
+}
