29efb5a97b0a0c250ce075bc047f765334115ea1
Eric Peyton <ea.peyton@gmail.com>
2015-04-14 15:34:20 -0400

    Add playCard method to JeezSocket

--- mobile/Cards/app/src/main/java/cards/seniordesign/com/cards/api/JeezSocket.java
+++ mobile/Cards/app/src/main/java/cards/seniordesign/com/cards/api/JeezSocket.java
@@ -1,22 +1,19 @@
 package cards.seniordesign.com.cards.api;
 
-import android.app.Activity;
 import android.util.Log;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
-import org.java_websocket.WebSocketImpl;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft_10;
-import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import cards.seniordesign.com.cards.Dialog;
+import cards.seniordesign.com.cards.game.Game;
+import cards.seniordesign.com.cards.models.Card;
 import cards.seniordesign.com.cards.models.Room;
 import cards.seniordesign.com.cards.models.User;
 import cards.seniordesign.com.cards.models.response.StartGameResponse;
@@ -32,14 +29,16 @@ public class JeezSocket {
     private static final String HOST_STARTED_GAME = "host started game";
     private static final String USER_NOT_HOST = "user is not host";
     private static final String GAME_BEING_PLAYED = "game is already being played";
+    private static final String PLAY_CARD = "play card";
+    private static final String USER_HAS_PLAYED = "user has played";
 
-    private Activity activity;
+    private Game game;
     private User currentUser;
     private Room currentRoom;
     private Socket webSocket;
 
-    public JeezSocket(Activity activity, User currentUser, Room currentRoom) {
-        this.activity = activity;
+    public JeezSocket(Game game, User currentUser, Room currentRoom) {
+        this.game = game;
         this.currentUser = currentUser;
         this.currentRoom = currentRoom;
         setupWebSocket();
@@ -52,9 +51,10 @@ public class JeezSocket {
             throw new RuntimeException(e);
         }
         webSocket.on(USER_JOINED, onUserJoined);
-        webSocket.on(HOST_STARTED_GAME, new NotifyListener("The game has started."));
+        webSocket.on(HOST_STARTED_GAME, onGameStarted);
         webSocket.on(USER_NOT_HOST, new NotifyListener("The user is not the host."));
         webSocket.on(GAME_BEING_PLAYED, new NotifyListener("The game is already being played."));
+        webSocket.on(USER_HAS_PLAYED, new NotifyListener("User played card"));
 
         webSocket.connect();
         webSocket.emit(SETUP_SOCKET, JeezConverter.toJson(currentUser));
@@ -63,14 +63,16 @@ public class JeezSocket {
     private ResponseListener<User> onUserJoined = new ResponseListener<User>(User.class) {
         @Override
         public void callOnUi(User response) {
-            Dialog.showNotification(activity, currentUser.getName() + " has joined the room.");
+            Dialog.showNotification(game, response.getName() + " has joined the room.");
         }
     };
 
     private ResponseListener<StartGameResponse> onGameStarted = new ResponseListener<StartGameResponse>(StartGameResponse.class) {
         @Override
         public void callOnUi(StartGameResponse response) {
-            Dialog.showNotification(activity, "Game has started.");
+            Log.i(getClass().getName(), "Object:" + JeezConverter.toJson(response).toString());
+            Dialog.showNotification(game, "Game has started.");
+            game.goToGameplay(response);
         }
     };
 
@@ -83,10 +85,21 @@ public class JeezSocket {
         webSocket.off(USER_JOINED, onUserJoined);
     }
 
+    public void playCard(Card card) {
+        try {
+            Log.i(this.getClass().getName(), "Played card:" + card.getId());
+            JSONObject cardObj =  new JSONObject();
+            cardObj.put("cardId", card.getId());
+            webSocket.emit(PLAY_CARD, cardObj);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private abstract class UiListener implements Emitter.Listener {
         @Override
         public void call(final Object... args) {
-            activity.runOnUiThread(new Runnable() {
+            game.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     callOnUi(args);
@@ -122,7 +135,7 @@ public class JeezSocket {
 
         @Override
         public void callOnUi(Object... args) {
-            Dialog.showNotification(activity, message);
+            Dialog.showNotification(game, message);
         }
     }
 
