5b03bb22ab231df0c9daac2d9035e86ab7864ec8
Ben Schmaltz <benschmaltz@gmail.com>
2015-04-16 03:39:50 -0400

    order cards

--- web/test/end-to-end/websocket/new-round.js
+++ web/test/end-to-end/websocket/new-round.js
@@ -68,68 +68,21 @@ describe("Clients recieve 'new round'",function(){
         lastResponse.should.have.property('round').with.property('state');
         lastResponse.round.state.should.be.exactly('waiting for players');
 
-        done();
-      })
-    });
-  });
-
-  it('players should have cards replaced', function(done){
-    var startGameEvents = [
-      {
-        sender: 0,
-        sendKey: 'start game',
-        resKey: 'host started game'
-      }
-    ];
-
-    websocketHelper.createClients()
-    .then(websocketHelper.createCards)
-    .then(websocketHelper.connectClients)
-    .then(websocketHelper.waitForEvents.bind({events: startGameEvents}))
-    .then(websocketHelper.updateClientsCards)
-    .then(function(clients){
-      var playCardEvents = [
-        {
-          sender:1,
-          sendKey: 'play card',
-          sendMsg: {cardId: clients[1].cards[0].id},
-          resKey: 'user has played'
-        },
-        {
-          sender:2,
-          sendKey: 'play card',
-          sendMsg: {cardId: clients[2].cards[0].id},
-          resKey: 'waiting for judge'
-        },
-        {
-          sender:0,
-          sendKey: 'choose winning card',
-          sendMsg: {
-            winner: clients[2].id,
-            winningCard: clients[2].cards[0].id
-          },
-          resKey: 'round review'
-        },
-        {
-          sender: 0,
-          sendKey: 'blank',
-          sendMsg: {},
-          resKey: 'new round'
-        }
-      ];
-      var runPlayCardEvents = websocketHelper.waitForEvents.bind({events: playCardEvents});
-
-      runPlayCardEvents(clients)
-      .then(function(clients){
-        var lastResponse = clients[0].lastResponse;
-        console.log("original cards=", clients[1].cards);
-        console.log("played card=", clients[1].cards[0]);
         websocketHelper.getClientsCards(2)
         .then(function(cards){
-          console.log("next round cards=", cards);
-          console.log("lengths before, after", clients[1].cards.length, cards.length);
+          console.log(clients[1].cards);
+          console.log(cards);
+          var a =[];
+          var b =[];
+          for(var i=0; i<cards.length; i++){
+            a.push(clients[1].cards[i].text);
+            b.push(cards[i].text);
+          }
+          console.log(a);
+          console.log(b);
+          clients[1].cards.length.should.equal(cards.length)
           done();
-        })
+        });
       })
     });
   });
